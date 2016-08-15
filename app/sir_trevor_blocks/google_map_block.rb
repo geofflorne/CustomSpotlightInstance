b318c5c11ec41e2d61f20e1ee66b89b1b05a7d34
Geoff <geo.lorne@gmail.com>
2016-08-15 12:18:24 -0700

    Tweaks to solr query for map; renamed screenshot button

--- app/sir_trevor_blocks/google_map_block.rb
+++ app/sir_trevor_blocks/google_map_block.rb
@@ -1,23 +1,27 @@
 class GoogleMapBlock < SirTrevorRails::Block
 
   #get items from solr to display on the map block
+  #search value is from block
   def getsolr
     items = Array.new
     solr = RSolr.connect :url => 'http://localhost:8983/solr/blacklight-core'
     res = solr.get 'select', :params => {
       :q=>self.search.gsub(',',' OR '),
       :start=>0,
-      :rows=>500,
-      :fl=> ['id', 'exhibit_test-2_public_bsi', 'thumbnail_url_ssm', 'full_title_tesim', 'spotlight_upload_dc.Coverage-Spatial.Location_tesim', 'spotlight_upload_dc.Coverage-Spatial.Location_ftesim', 'spotlight_upload_dc.description_tesim']
+      :rows=>1000,
+      :fl=> ['id', 'exhibit_test-2_public_bsi', 'thumbnail_url_ssm', 'full_title_tesim', 'spotlight_upload_dc.Coverage-Spatial.Location_tesim', 'spotlight_upload_dc.Coverage-Spatial.Location_ftesim', 'spotlight_upload_dc.description_tesim'],
+      :fq=> 'spotlight_upload_dc.Coverage-Spatial.Location_tesim:[* TO *]',
+      :wt=> 'ruby'
     }
 
     #only return items that have a valid lat/long in its location field
     res['response']['docs'].each do |r|
-      if r['spotlight_upload_dc.Coverage-Spatial.Location_tesim'].present? && r['spotlight_upload_dc.Coverage-Spatial.Location_tesim'][0].match(/(-?\d{1,3}\.\d+)/) != nil
+      if r['spotlight_upload_dc.Coverage-Spatial.Location_tesim'][0].match(/(-?\d{1,3}\.\d+)/) != nil
         items.push(r)
       end
     end
-    items
+    #sort items by title
+    items.sort!{|a,b| a['full_title_tesim'][0].downcase <=> b['full_title_tesim'][0].downcase}
   end
 
 
